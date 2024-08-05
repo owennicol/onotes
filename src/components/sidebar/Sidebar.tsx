d4a48e23830bc7a356811fbc72ae2848552cafe8
Owen Nicol <owennicol@users.noreply.github.com>
2024-08-05 14:19:49 +0100

    Add sidebar option

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { NewDocumentButton } from "../newDocument/NewDocumentButton";
 import {
   Sheet,
@@ -11,37 +13,109 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { useUser } from "@clerk/nextjs";
+import {
+  collectionGroup,
+  DocumentData,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "@/lib/firebase";
+import { SidebarOption } from "./SidebarOption";
+
+interface RoomDocument extends DocumentData {
+  userId: string;
+  roomId: string;
+  role: "owner" | "editor";
+  createdAt: string;
+}
 
 export function Sidebar() {
+  const { user } = useUser();
+  const [groupedData, setGroupedData] = useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({
+    owner: [],
+    editor: [],
+  });
+
+  const [data, loading, error] = useCollection(
+    user &&
+      query(
+        collectionGroup(db, "rooms"),
+        where("userId", "==", user.emailAddresses[0].toString())
+      )
+  );
+
+  useEffect(() => {
+    if (!data) return;
+
+    const grouped = data.docs.reduce<{
+      owner: RoomDocument[];
+      editor: RoomDocument[];
+    }>(
+      (acc, curr) => {
+        const roomData = curr.data() as RoomDocument;
+        const newObj = { id: curr.id, ...roomData };
+
+        if (roomData.role === "owner") {
+          acc.owner.push(newObj);
+        } else {
+          acc.editor.push(newObj);
+        }
+
+        return acc;
+      },
+      {
+        owner: [],
+        editor: [],
+      }
+    );
+
+    setGroupedData(grouped);
+  }, [data]);
+
   const menuOptions = (
     <>
-    <NewDocumentButton />
-    {/* my docs */}
+      <NewDocumentButton />
+      {/* my docs */}
+      <div className="flex py-4 flex-col space-y-4 md:max-w-36">
+        {groupedData.owner.length === 0 ? (
+          <h2 className="text-gray-500 font-semibold text-sm">
+            No documents found
+          </h2>
+        ) : (
+          <>
+            <h2 className="text-gray-500 font-semibold text-sm">My Documents</h2>
+            {groupedData.owner.map((doc) => (
+              <SidebarOption key={doc.id} id={doc.id} href={`/doc/${doc.id}`} />
+            ))}
+          </>
+        )}
+      </div>
 
-    {/* shared with me */}
+      {/* shared with me */}
     </>
-
-
-  )
+  );
   return (
     <div className="p-2 md:p-5 bg-gray-200 relative">
       <div className="md:hidden">
-        <Sheet >
-          <SheetTrigger><MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} /></SheetTrigger>
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
+          </SheetTrigger>
           <SheetContent side="left">
             <SheetHeader>
               <SheetTitle>Menu</SheetTitle>
-              <div>
-                {menuOptions}
-              </div>
+              <div>{menuOptions}</div>
             </SheetHeader>
           </SheetContent>
         </Sheet>
       </div>
 
-      <div className="hidden md:inline">
-        {menuOptions}
-      </div>
+      <div className="hidden md:inline">{menuOptions}</div>
     </div>
   );
 }
---
+++ src/components/sidebar/SidebarOption.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { db } from "@/lib/firebase";
+import { doc } from "firebase/firestore";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+export function SidebarOption({ href, id }: { href: string; id: string }) {
+  const [data, loading, error] = useDocumentData(doc(db, "documents", id));
+  const pathname = usePathname();
+  const isActive = href.includes(pathname) && pathname !== "/";
+
+  if (!data) return null;
+
+  return (
+    <Link
+      href={href}
+      className={`border p-2 rounded-md ${
+        isActive ? "bg-gray-300 font-bold border-black" : "border-gray-400"
+      }`}
+    >
+      <p className="truncate">{data?.title}</p>
+    </Link>
+  );
+}
